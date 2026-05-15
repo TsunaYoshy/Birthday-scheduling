2ff143ecb13674a5249d84aeeeda0c32facc310e
Matheus Pezzotti <matheus.pezzotti@gmail.com>
2026-05-15 02:53:54 -0300

    Adiciona novas rotas e serviços para agendamento e envio de mensagens de aniversário, incluindo suporte a configuração de ambiente

--- app/main.py
+++ app/main.py
@@ -3,7 +3,13 @@ from fastapi import FastAPI
 from app.database import Base
 from app.database import engine
 
+from app.models.birthday_client import BirthdayClient
+from app.models.sent_birthday import SentBirthday
+
 from app.routes.sync import router as sync_router
+from app.routes.birthdays import router as birthdays_router
+from app.routes.send import router as send_router
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -12,7 +18,8 @@ app = FastAPI(
 )
 
 app.include_router(sync_router)
-
+app.include_router(birthdays_router)
+app.include_router(send_router)
 
 @app.get("/")
 def home():
